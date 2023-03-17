153b05ff5d5c58d3e4e7012d5adedce9dc3050f4
amrit578singh@gmail.com <meanstack9716>
2023-03-17 17:35:10 +0530

    add node cron job and changes in database add only unique data

--- controller/jsonData.js
+++ controller/jsonData.js
@@ -1,9 +1,10 @@
 const xml2js = require("xml2js");
 const https = require("https");
+const cron = require("node-cron");
 const { connectToDb, insertDocuments } = require("../Database/db");
 
-const createJsonData = async (req, res) => {
-  try {
+const createJsonData = async () => {
+  return new Promise((resolve, reject) => {
     https.get(process.env.API, (response) => {
       let data = "";
       response.on("data", (result) => {
@@ -14,28 +15,27 @@ const createJsonData = async (req, res) => {
         xml2js.parseString(data, async (err, result) => {
           if (err) {
             console.error(err);
-            res.status(401).send({ message: err });
+            reject(err);
           }
 
           const items = result.rss.channel[0].item;
-          const { collection, client } = await connectToDb();
-
-          try {
-            const insertedData = await insertDocuments(collection, items);
-            res.status(200).send({ insertedData });
-          } catch (error) {
-            console.error(error);
-            res.status(401).send({ message: "Error" });
-          } finally {
-            await client.close();
-          }
+          resolve({ items });
         });
       });
     });
-  } catch (error) {
-    console.log(error);
-    res.status(401).send({ message: "Error" });
-  }
+  });
 };
+
+cron.schedule("*/20 * * * * *", async (req,res) => {
+  try {
+    const { collection, client } = await connectToDb();
+    const response = await createJsonData();
+    await insertDocuments(collection, response.items);
+    await client.close();
+    console.log("API data updated successfully.");
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 module.exports = { createJsonData };
